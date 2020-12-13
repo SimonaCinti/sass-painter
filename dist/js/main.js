2664c6e3d762b3c1b3437890e1a9945406d05a00
Simona Cinti <simona.cinti2@gmail.com>
2020-12-13 15:12:42 +0100

    gallery

--- dist/js/main.js
+++ dist/js/main.js
@@ -93,7 +93,30 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-console.log('hello js');
+var app = new Vue({
+  el: '#app',
+  data: {
+    // Slide gallery img
+    indexPhoto: 0,
+    gallery: ['./img/gallery-01.jpg', './img/gallery-02.jpg', './img/gallery-03.jpg', './img/gallery-04.jpg', './img/gallery-05.jpg', './img/gallery-06.jpg']
+  },
+  methods: {
+    nextPhoto: function nextPhoto() {
+      this.indexPhoto++;
+
+      if (this.indexPhoto > this.gallery.length - 1) {
+        this.indexPhoto = 0;
+      }
+    },
+    prevPhoto: function prevPhoto() {
+      this.indexPhoto--;
+
+      if (this.indexPhoto < 0) {
+        this.indexPhoto = this.gallery.length - 1;
+      }
+    }
+  }
+});
 
 /***/ }),
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,31 @@
-console.log('hello js');
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        // Slide gallery img
+        indexPhoto: 0,
+        gallery: [
+            './img/gallery-01.jpg',
+            './img/gallery-02.jpg',
+            './img/gallery-03.jpg',
+            './img/gallery-04.jpg',
+            './img/gallery-05.jpg',
+            './img/gallery-06.jpg',
+        ],
+    },
+    methods: {
+        nextPhoto() {
+            this.indexPhoto++
+            if (this.indexPhoto > this.gallery.length - 1) {
+                this.indexPhoto = 0
+            }
+        },
+        prevPhoto() {
+            this.indexPhoto--
+            if (this.indexPhoto < 0) {
+                this.indexPhoto = this.gallery.length - 1
+            }
+        },
+    },
+
+})
